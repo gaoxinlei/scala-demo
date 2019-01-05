e90eac27896dec264b277ebfcf86e50e044e46a3
gaoxinlei <xinleigao@github.com>
2019-01-05 17:41:21 +0800

    演示默认apply方法和update方法的应用以及map取optional

--- src/main/scala/com/example/scala/HelloWorld.scala
+++ src/main/scala/com/example/scala/HelloWorld.scala
@@ -3,6 +3,15 @@ package com.example.scala
 object HelloWorld {
 
   def main(args: Array[String]): Unit = {
-    println("Hello Scala!")
+    println("Hello Scala!"(4))//等同于"Hello Scala".apply(4)
+
+    val arr = new Array[Int](3)
+    arr(1)=2//等同于arr.update(1,2)
+    arr.update(2,3)
+    println(arr.mkString(","))
+
+    val map = Map("a"->"alice","b"->"tom")//构建map测试optional
+    println(map.get("a"))
+    println(map.get("c"))
   }
 }
