770dbe094b55c33e1b2afa2307027ab5e0328d8a
gaoxinlei <xinleigao@github.com>
2019-01-05 17:53:47 +0800

    if else 赋值演示

--- src/main/scala/com/example/scala/HelloWorld.scala
+++ src/main/scala/com/example/scala/HelloWorld.scala
@@ -13,5 +13,24 @@ object HelloWorld {
     val map = Map("a"->"alice","b"->"tom")//构建map测试optional
     println(map.get("a"))
     println(map.get("c"))
+
+    //if else相当于函数，可以有返回值并赋给变量，也同时可使用变量类型推断，即if与else返回的值可以是不同类型。
+    val a1 = 18
+    val ifEls1 =
+      if (a1>20){
+        "大于20"
+      }else{
+        "小于20"
+      }
+    println(ifEls1)
+
+    val a2 = 22
+    val ifEls2 =
+      if(a2>20){
+        a1
+      }else{
+        "小于20"
+      }
+    println(ifEls2)
   }
 }
